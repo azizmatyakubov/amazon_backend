a72b76095245e9d2cea1df7642e1a8b9e2c0f700
Amin Atashkhayer <amin.atashin@gmail.com>
2022-04-29 15:43:07 +0200

    add

--- src/services/product/index.js
+++ src/services/product/index.js
@@ -1,80 +1,92 @@
-import express from "express"
-import createError from "http-errors"
-import productmodel from "./model.js"
+import express from "express";
+import createError from "http-errors";
+import productmodel from "./model.js";
+import reviewsModel from "./model.js";
 // =====================================
-const productRouter = express.Router()
+const productRouter = express.Router();
 // =====================================
 productRouter.post("/", async (req, res, next) => {
   try {
-    const product = await productmodel(req.body)
-    const { _id } = await product.save()
-    res.send({ _id })
+    const product = await productmodel(req.body);
+    const { _id } = await product.save();
+    res.send({ _id });
   } catch (error) {
-    next(createError(404, "Product page not found!"))
+    next(createError(404, "Product page not found!"));
   }
-})
+});
 // =====================================
 productRouter.get("/", async (req, res, next) => {
   try {
-    const getproduct = await productmodel.find().populate({ path: "reviews", select: "_id comment rate" })
+    const getproduct = await productmodel
+      .find()
+      .populate({ path: "reviews", select: "_id comment rate" });
 
-    res.send(getproduct)
+    res.send(getproduct);
   } catch (error) {
-    next(createError(404, "Product page not found!"))
+    next(createError(404, "Product page not found!"));
   }
-})
+});
 // =====================================
 productRouter.get("/:productId", async (req, res, next) => {
   try {
-    const product = await productmodel.findById(req.params.productId)
+    const product = await productmodel.findById(req.params.productId);
 
-    res.send(product)
+    res.send(product);
   } catch (error) {
-    next(createError(404, `Product with ${req.params.productId} not found!`))
+    next(createError(404, `Product with ${req.params.productId} not found!`));
   }
-})
+});
 // =====================================
 productRouter.put("/:productId", async (req, res, next) => {
   try {
-    const modify = await productmodel.findByIdAndUpdate(req.params.productId, req.body, {
-      new: true,
-      runValidators: true,
-    })
-    res.send()
+    const modify = await productmodel.findByIdAndUpdate(
+      req.params.productId,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    res.send();
   } catch (error) {
-    next(createError(404, `Product with Id${req.params.productId} not found!`))
+    next(createError(404, `Product with Id${req.params.productId} not found!`));
   }
-})
+});
 // =====================================
 productRouter.delete("/:productId", async (req, res, next) => {
   try {
-    const delet = await productmodel.findByIdAndDelete(req.params.productId)
-    res.send()
+    const delet = await productmodel.findByIdAndDelete(req.params.productId);
+    res.send();
   } catch (error) {
-    next(createError(404, `Product with Id${req.params.productId} not found!`))
+    next(createError(404, `Product with Id${req.params.productId} not found!`));
   }
-})
+});
 
-
-productRouter.post('/:productId/reviews', async(req, res, next) => {
+productRouter.post("/:productId/reviews", async (req, res, next) => {
   try {
-    const newReview = {...req.body, date: new Date()}
-    console.log(newReview)
-    const updatedProduct = await productmodel.findByIdAndUpdate(req.params.productId, {$push: {reviews: newReview}}, {new: true, runValidators: true})
-    if(updatedProduct) {
-      res.status(200).send(updatedProduct)
+    const newReview = await reviewsModel.findById(req.body.reviewId);
+
+    const convertReview = { ...newReview.toObject(), date: new Date() };
+
+    const updatedProduct = await productmodel.findByIdAndUpdate(
+      req.params.productId,
+      { $push: { reviews: convertReview } },
+      { new: true, runValidators: true }
+    );
+    if (updatedProduct) {
+      res.status(200).send(updatedProduct);
     } else {
-      next(createError(404, `Product with id ${req.params.productId} not found`))
+      next(
+        createError(404, `Product with id ${req.params.productId} not found`)
+      );
     }
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 productRouter.get("/:productId/reviews", async (req, res, next) => {
   try {
-   
-
     // const product = await productmodel.findById(req.params.productId).populate({ path: "reviews", select: "_id comment rate " })
     // if (product) {
     //   res.send(product.reviews)
@@ -82,10 +94,10 @@ productRouter.get("/:productId/reviews", async (req, res, next) => {
     //   next(createError(404, `Product with Id${req.params.productId} not found!`))
     // }
   } catch (error) {
-    next(createError(404, `Product with Id${req.params.productId} not found!`))
+    next(createError(404, `Product with Id${req.params.productId} not found!`));
   }
-})
+});
 // =====================================
-export default productRouter
+export default productRouter;
 
 // 626bd86639772a270c0f5d2a
--- src/services/reviews/index.js
+++ src/services/reviews/index.js
@@ -1,67 +1,73 @@
-import express from "express"
-import createError from "http-errors"
-import ReviewsModel from "./model.js"
+import express from "express";
+import createError from "http-errors";
+import ReviewsModel from "./model.js";
 
-const reviewsRouter = express.Router()
+const reviewsRouter = express.Router();
 
 ///////////
-reviewsRouter.post("/:productId/reviews", async (req, res, next) => {
+reviewsRouter.post("/", async (req, res, next) => {
   try {
-    const newReview = {...req.body, date: Date()}
-    console.log(newReview)
-
+    const newReview = await ReviewsModel(req.body);
+    const { _id } = await newReview.save();
+    res.send({ _id });
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 ///////////
 reviewsRouter.get("/", async (req, res, next) => {
   try {
-    const review = await ReviewsModel.find()
+    const review = await ReviewsModel.find();
     if (review) {
-      res.status(200).send(review)
+      res.status(200).send(review);
     } else {
-      next(createError(404, "There isn't any review available"))
+      next(createError(404, "There isn't any review available"));
     }
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 ///////////
 reviewsRouter.get("/:reviewId", async (req, res, next) => {
   try {
     // const updatedReview = await ReviewsModel.findByIdAndUpdate(req.params.blogId, { $push: { comments: req.body } }, { new: true })
 
     if (updatedReview) {
-      res.status(201).send(updatedReview)
+      res.status(201).send(updatedReview);
     } else {
-      next(createError(404, "There isn't any review available"))
+      next(createError(404, "There isn't any review available"));
     }
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 ///////////
 reviewsRouter.put("/:reviewId", async (req, res, next) => {
   try {
-    const updatedReview = await ReviewsModel.findByIdAndUpdate(req.params.reviewId, req.body, { new: true, runValidators: true })
-    res.send(updatedReview)
+    const updatedReview = await ReviewsModel.findByIdAndUpdate(
+      req.params.reviewId,
+      req.body,
+      { new: true, runValidators: true }
+    );
+    res.send(updatedReview);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 ///////////
 reviewsRouter.delete("/:reviewId", async (req, res, next) => {
   try {
-    const reviewToDelete = await ReviewsModel.findByIdAndDelete(req.params.reviewId)
+    const reviewToDelete = await ReviewsModel.findByIdAndDelete(
+      req.params.reviewId
+    );
     if (reviewToDelete) {
-      res.status(201).send("RIP")
+      res.status(201).send("RIP");
     } else {
-      next(createError(404, "There isn't any review to delete here"))
+      next(createError(404, "There isn't any review to delete here"));
     }
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-export default reviewsRouter
+export default reviewsRouter;
