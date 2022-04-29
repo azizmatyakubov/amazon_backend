f1e72a2a460428a7335b4d373c6fbc1525e911c3
AZIZ <a.matyaqubov1471@gmail.com>
2022-04-29 10:53:30 +0200

    initial commit

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import listendpoints from 'express-list-endpoints'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+const server = express()
+const port = process.env.PORT || 3002
+
+
+server.use(cors())
+server.use(express.json())
+
+
+// server.use('/product')
+// server.use('/reviews')
+
+
+mongoose.connect(process.env.MONGO_CONNECTION)
+mongoose.connection.on('connected', ()=> {
+    console.log('Successfully connected to DB')
+
+    server.listen(port, ()=>{
+        console.log(`Server is running on port ${port}`)
+        console.table(listendpoints(server))
+    })
+})
+
+
